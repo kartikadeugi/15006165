ce453cab5511ecbc6a66c5261917a1daf3714fbc
Kartika Deugi <deugikartika@gmail.com>
2018-12-29 22:45:01 +0000

    Sprint Cycle 4

--- mobilecomputing/PlayerTouch.swift
+++ mobilecomputing/PlayerTouch.swift
@@ -9,26 +9,47 @@
 import UIKit
 
 class PlayerTouch: UIImageView {
-    let topBorder = UIScreen.main.bounds.minY + CGFloat(150)
+    let topBorder = UIScreen.main.bounds.minY + CGFloat(50)
 
     let bottomBorder = UIScreen.main.bounds.maxY - CGFloat(150)
-    var getAvatar: avatar?
+    var myDelegate: avatarDelegate?
+
+    var startLocation: CGPoint?
     
     
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        startLocation = touches.first?.location(in: self)
+    }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let startLocation = touch.location(in: self.superview)
-            
-            
-            if(startLocation.y < bottomBorder && startLocation.y > topBorder) {
-                self.center.x = startLocation.x
-                self.center.y = startLocation.y
-                self.getAvatar?.getAvatar()
-            }
-        }
+        
+        let currentLocation = touches.first?.location(in: self)
+        let dx = currentLocation!.x - startLocation!.x
+        let dy = currentLocation!.y - startLocation!.y
+        self.center = CGPoint(x: self.center.x+dx, y: self.center.y+dy)
+        self.myDelegate?.getAvatar()
+
     }
+    
+
+    
+    
+    
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if let touch = touches.first {
+//            let startLocation = touch.location(in: self.superview)
+//
+//
+//            if(startLocation.y < bottomBorder && startLocation.y > topBorder) {
+//                self.center.x = startLocation.x
+//                self.center.y = startLocation.y
+//                self.getAvatar?.getAvatar()
+//
+//            }
+//
+//        }
+//    }
 
 
     
--- mobilecomputing/ViewController.swift
+++ mobilecomputing/ViewController.swift
@@ -7,69 +7,166 @@
 //
 
 import UIKit
-protocol avatar {
+protocol avatarDelegate {
     func getAvatar()
 }
 
-class ViewController: UIViewController{
-
+class ViewController: UIViewController, UICollisionBehaviorDelegate, avatarDelegate {
+ 
     
-
+    @IBOutlet weak var scoreDisplay: UITextView!
     @IBOutlet weak var gameBackground: UIImageView!
-    
-    
     @IBOutlet weak var treeScenery: UIImageView!
     var dynamicAnimator: UIDynamicAnimator!
-    var timer: Timer!
     
     var birdImages = Array<UIImage>()
-    let collisionBehaviour = UICollisionBehavior()
-
-
+    var coinImages = Array<UIImage>()
+    
+    
+    var gameOver = false
+    var obstacleTimer: Timer!
+    var coinTimer: Timer!
+    var playerScore = 0
+    
+    var countTimer: Timer!
+    var countDownTotal = 60
+    
     @IBOutlet weak var player: PlayerTouch!
     
+    var collisionBehaviour: UICollisionBehavior!
+    
+   
+    func startTimer() {
+        obstacleTimer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true, block: { (T) in
+            self.playerScore = self.playerScore + 2
+            self.createObstacles()
+            
+        } )
+        
+        coinTimer = Timer.scheduledTimer(withTimeInterval: 2.5, repeats: true, block: { (T) in
+            self.createCoins()
+        } )
+        
+        countTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    
+    // Called when a collision, between a dynamic item and a collision boundary, has begun.
+    // In order for this method to work, it must conform with the UICollisionDelegate protocol.
+    func collisionBehavior(_ behavior: UICollisionBehavior, beganContactFor item: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying?, at p: CGPoint) {
+        playerScore = playerScore - 2
+        print("Collision")
+    }
+    
+    @objc func updateTimer() {
+        
+
+        scoreDisplay.text = "🏆\(playerScore)"
+        print(countDownTotal)
+        
+        if(countDownTotal != 0) {
+            countDownTotal = countDownTotal - 1
+        } else {
+            gameOver = true
+            countTimer.invalidate()
+            obstacleTimer.invalidate()
+            coinTimer.invalidate()
+        }
+    }
+    
+    
+    func getAvatar() {
+        collisionBehaviour.removeAllBoundaries()
+
+        collisionBehaviour.addBoundary(withIdentifier: "barrier" as NSCopying, for: UIBezierPath(rect: player.frame))
+    }
+    
+ 
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let value = UIInterfaceOrientation.landscapeLeft.rawValue
         
         // Initialising the dynamic animator
+        player.myDelegate = self
+
         dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        collisionBehaviour = UICollisionBehavior()
+        collisionBehaviour.collisionDelegate = self
 
         UIDevice.current.setValue(value, forKey: "orientation")
         buildBackground()
-
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (T) in self.createObstacles() } )
-
+        
+        startTimer()
         
         
     }
     
-    @objc func createObstacles() {
+
+   
+    func createCoins() {
         
+        let randomPosition = Double.random(in: 0...150)
+        let createCoins = UIImageView(image:nil)
+        createCoins.image = UIImage.animatedImage(with: coinImages, duration: 0.5)
+        createCoins.frame = CGRect(x:0, y:randomPosition, width: 40, height: 40)
+        createCoins.center.x = UIScreen.main.bounds.maxX - 150
+        createCoins.tag = 1
+        let coinDynamics = UIDynamicItemBehavior(items: [createCoins])
+        let randomSpeed = Double.random(in: -300 ... -100)
+        coinDynamics.addLinearVelocity(CGPoint(x: randomSpeed, y:0 ),for: createCoins)
+        self.view.addSubview(createCoins)
+        dynamicAnimator.addBehavior(coinDynamics)
         
-        let randomPosition = Double.random(in: -150...150)
+        let coinCollision = UICollisionBehavior(items: [createCoins])
+        dynamicAnimator.addBehavior(coinCollision)
+        
+        coinCollision.action = {
+
+
+            if(self.player.frame.contains(createCoins.frame)) {
+                createCoins.removeFromSuperview()
+                self.dynamicAnimator.removeBehavior(coinCollision)
+                self.playerScore = self.playerScore + 1
+            }
+
+        }
+        
+    }
+    
+
+    
+    
+    func createObstacles() {
+        
+        let randomPosition = Double.random(in: 0...150)
         
         
         let crowObstacle = UIImageView(image:nil)
+        crowObstacle.tag = 0
         crowObstacle.image = UIImage.animatedImage(with: birdImages, duration: 0.5)
         crowObstacle.frame = CGRect(x:0, y:randomPosition, width: 100, height: 100)
         crowObstacle.center.x = UIScreen.main.bounds.maxX - 150
         
         // creating dynamic behaviour to crow obstances
-        let dynamics = UIDynamicItemBehavior(items: [crowObstacle
+        let crowDynamics = UIDynamicItemBehavior(items: [crowObstacle
             ])
     
         let randomSpeed = Double.random(in: -500 ... -200)
-        dynamics.addLinearVelocity(CGPoint(x: randomSpeed, y: 0), for: crowObstacle)
-        gameBackground.addSubview(crowObstacle)
+        crowDynamics.addLinearVelocity(CGPoint(x: randomSpeed, y: 0), for: crowObstacle)
+        crowDynamics.elasticity = 0.5
+
+
+        self.view.addSubview(crowObstacle)
         collisionBehaviour.addItem(crowObstacle)
-        collisionBehaviour.addItem(player)
-        dynamicAnimator.addBehavior(dynamics)
+
+        getAvatar()
+
+        dynamicAnimator.addBehavior(crowDynamics)
         dynamicAnimator.addBehavior(collisionBehaviour)
 
-   
-        
     }
     
     
@@ -84,6 +181,10 @@ class ViewController: UIViewController{
             
             if(i<10) {
                 birdImages.append(UIImage(named:"bird\(i).png")!)
+            }
+            
+            if(i<7) {
+                coinImages.append(UIImage(named:"coin\(i).png")!)
             }
             
             if(i<18) {
